c331d655de1fd5ce9985a67d0d18b37246489312
Adriano Chacheto <adrianochacheto@gmail.com>
2021-02-03 00:03:50 -0300

    Prática de hoje, relebrando comandos js

---
+++ js/main.js
@@ -0,0 +1,53 @@
+//alert('meu primeiro alerta js.');
+/* var nome = "Adriano";
+var idade = 10;
+var idade2 = 29;
+//alert(idade + idade2 + " concatenação");
+//alert("concatenação " + idade + idade2);
+console.log(nome);
+console.log(idade + idade2); */
+
+/* var lista = ['maçã','pêra','laranja'];
+lista.push("uva");
+console.log(lista.pop());
+console.log(lista);
+console.log(lista.length);
+console.log(lista.reverse());
+console.log(lista.toString());
+console.log(lista.join(" - ")); */
+
+/* var fruta = { nome: "maçã", cor: "vermelha" };
+console.log(fruta);
+console.log(fruta.cor); */
+
+/* var frutas = [{ nome: "maçã", cor: "vermelha" }, { nome: "uva", cor: "roxa" }];
+console.log(frutas);
+console.log(frutas[1].cor); */
+
+/* var idade = prompt("Qual a sua idade?", "19");
+if (idade >= 18) {
+    console.log("Maior de idade");
+}
+else {
+    console.log("Menor de idade");
+};
+
+console.log(idade); */
+
+/* var count = 0;
+while (count <= 5) {
+    console.log(count);
+    count++;
+}; */
+
+/* for(var count = 0; count <= 5; count++) {
+    console.log(count);
+}; */
+
+/* var d = new Date();
+console.log(d);
+console.log(d.getMonth()+1);
+console.log(d.getMinutes());
+console.log(d.getDay()); */
+
+
